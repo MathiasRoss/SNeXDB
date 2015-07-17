b27b487c7f60634e92f23a47152549d38333e66f
Mathias Ross <Mathias_Ross@msn.com>
2015-07-17 17:45:33 -0500

    increased fields in export

--- export.php
+++ export.php
@@ -3,9 +3,9 @@ include 'connect.php';
 include 'calculations.php';
 include 'search.php';
 
-$string = " \"Object Name\", \"Type\", \"Age\", \"Flux\",\"Luminosity\" \n";
+$string = " \"obsID\", \"name\", \"type\",\"dateExploded\",\"distance\", \"age\", \"dateObserved\", \"instrument\", \"flux\", \"fluxErrL\",\"fluxErrH\",\"fluxEnergyL\",\"fluxEnergyH\",\"model\",\"lum\", \"fluxRef\",\"dateObservedRef\",\"dateExplodedRef\",\"distRef\" \n";
 foreach($result as $value){
-    $string = $string . $value["name"] . ",". $value["type"] . "," . $value['age'] . "," . $value["flux"] . ",". $value["lum"] . "\n";  
+    $string = $string . $value["obsID"] .','. $value["name"] . ",". $value["type"] . "," . $value['dateExploded'].','.$value['distance'].$value['age'] . "," . $value['dateObserved'].','.$value['instrument'].','.$value["flux"] . ",". $value['fluxErrL'].','.$value['fluxErrH'].','.$value['fluxEnergyL'].','.$value['fluxEnergyH'].','.$value['model'].','.$value["lum"] . ','.$value['fluxRef'].','.$value['dateObservedRef'].','.$value['dateExplodedRef'].','.$value['distRef']."\n";  
 }
 header('content-type: text/csv');
 header('content-disposition: attachment; filename="SNeXDB.csv"');
