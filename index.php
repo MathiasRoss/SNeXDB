2f099a8571acb3966b10e1b1101ff57decebf952
Mathias Ross <Mathias_Ross@msn.com>
2015-07-30 19:28:17 -0500

    made links visible

--- index.php
+++ index.php
@@ -6,18 +6,17 @@ include 'calculations.php';
 include 'header.php';
 
 include 'connect.php';
-
 include 'searchForm.php';
-include 'search.php';
-dispMem();
-include 'exportButton.php';
+if (isset($_GET['objid'])){
+    include 'search.php';
+    include 'exportButton.php';
 ?>
 <hr>
 <?php
-include 'table.php';
-dispMem();
-if(!isset($_GET["graph"])){
-    include 'plot.php';
+    include 'table.php';
+    if(!isset($_GET["graph"])){
+        include 'plot.php';
+    }
 }
 include 'footer.php';
 
